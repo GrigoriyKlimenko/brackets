02defecc8baf29245cb2934a727c609b981d4b38
GrigoryKlimenko <fageol46@gmail.com>
2019-03-24 08:27:54 +0300

    feat: solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+  let i;
+  let j;
+  let arrayBrackets = str.split('');
+  bracketsConfig = bracketsConfig.toString().replace(/,/g, '').split('');
+  for (i = 1; i < arrayBrackets.length; i++) {
+    if (i == 0){
+      i = 1;
+    }
+    for (j = 1; j < bracketsConfig.length; j +=2){
+      if (arrayBrackets[i] == bracketsConfig[j] && arrayBrackets[i - 1] == bracketsConfig[j - 1]){
+        arrayBrackets.splice(i-1, 2);
+        i-=2;
+        break;
+      }
+    }
+  }
+  if (arrayBrackets.length == 0){
+    return true;
+  } else {
+    return false;
+  }
 }
